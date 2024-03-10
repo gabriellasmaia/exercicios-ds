e3221dc7612755da3f0d7eb1db8fd22675566502
Gabriella Maia <maiagabriella18@gmail.com>
2024-03-09 21:31:02 -0400

    Adicionando a pasta de descritiva I

---
+++ Python/scripting/analise.py
@@ -0,0 +1,6 @@
+from extracao import extrair_dados
+from visualizacao import visualizar_dados
+
+dados = extrair_dados()
+
+grafico = visualizar_dados()
---
+++ Python/scripting/visualizacao.py
@@ -0,0 +1,13 @@
+import csv
+from sys import argv
+import pandas as pd
+import seaborn as sns
+
+def visualizar_dados():
+    df = pd.read_csv('./taxa-cdi.csv')
+
+    grafico = sns.lineplot(x=df['hora'], y=df['taxa'])
+    _ = grafico.set_xticklabels(labels=df['hora'], rotation=90)
+    grafico.get_figure().savefig(f"{argv[1]}.png")
+
+
